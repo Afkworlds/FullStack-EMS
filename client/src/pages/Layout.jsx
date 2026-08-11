2cb4731389c3004511f088512b518b86bc224e12
Afkworlds <AchyutBIM2080@Prime.edu.np>
2026-08-11 09:10:33 +0545

    Add sidebar navigation and update layout

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,14 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
 
 const Layout = () => {
   return (
     <div className="flex h-screen bg-gradient-to-r from-slate-50 via-white to-indigo-50/30">
-
-      {/* <aside className="w-64 bg-white shadow-md p-6"> */}
-        <p>
-          Sidebar
-        </p>
-      {/* </aside> */}
+      <Sidebar />
       <main className="flex-1 overflow-y-auto">
         <div className="p-4 pt-16 sm:p-6 sm:pt-6 lg:p-8 max-w-400 mx-auto">
           <Outlet />
@@ -16,7 +12,7 @@ const Layout = () => {
       </main>
 
     </div>
-  );
-};
+  )
+}
 
 export default Layout;
